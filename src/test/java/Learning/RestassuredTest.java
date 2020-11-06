62ec7981c4fb806a704c7ea80a7410c7f80e243f
shankar <shankar.sk@cred.club>
2020-11-06 18:12:03 +0530

    RestApi changes

--- src/test/java/Learning/RestassuredTest.java
+++ src/test/java/Learning/RestassuredTest.java
@@ -8,4 +8,15 @@ public class RestassuredTest {
     public void restapi(){
         System.out.println("writing rest assured api test cases");
     }
+
+    @Test
+    public void RestApi1(){
+        System.out.println("new changes needs to pushed");
+        System.out.println("changed for git 1");
+        System.out.println("chnaged 2 for github");
+    }
+    @Test
+    public void gitHub(){
+        System.out.println("git hubbbb ");
+    }
 }
